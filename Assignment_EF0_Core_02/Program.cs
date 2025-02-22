e3118ec507655e3f1cc652980406fcefceca9750
youssef_74 <youssefessam1293@gmail.com>
2025-02-22 23:49:57 +0200

    Add project files.

---
+++ Assignment_EF0_Core_02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_EF0_Core_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
